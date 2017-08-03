3155e98364afe4f04c97c7e878819c3bc14fa42b
Jamin <paranoimin@gmail.com>
2017-08-03 21:31:42 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    header('Content-type: text/html; charset=euc-kr');
+?>
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+
+        <link rel="stylesheet" href="./src/css/reset.css">
+        <link rel="stylesheet" href="./src/css/header.css">
+        <link rel="stylesheet" href="./src/css/basic.css">
+
+    </head>
+    <body>
+        <div id="wrap">
+        <?php include './src/assets/main/header.html'; ?>
+
+        <div id="content">
+
+        </div>
+
+        <?php include './src/assets/main/footer.html'; ?>
+
+        </div>
+    </body>
+</html>
